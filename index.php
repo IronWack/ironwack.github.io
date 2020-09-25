55dbce0c7be7b68f9531efffd7a5287c2facdb02
Duncan <duncan.mcb.ca@gmail.com>
2020-09-25 17:10:01 -0400

    php?

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include "header.php";?>
+
+<main role="main" class="flex-shrink-0">
+    <div class="container">
+        <h1 class="mt-5">Welcome to my page</h1>
+        <p class="lead">
+            My name is Duncan McBryan and I am a computer programmer analysis!
+        </p>
+    </div>
+</main>
+
+<?php include "footer.php";?>
+
